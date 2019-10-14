f9da00afdbdba4af904a4ae2aaa9c392b1381fb4
theone <xuguangyue@dtrobo.com>
2019-10-14 17:16:42 +0800

    实现redux

---
+++ demo01/src/example7/Example7.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ShowArea from './ShowArea';
+import Button from './Button';
+import { Color } from './color';
+
+function Example7() {
+    return (
+        <div>
+            <Color>
+                <ShowArea/>
+                <Button/>
+            </Color>
+        </div>
+    )
+}
+
+export default Example7
--- demo01/src/index.js
+++ demo01/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Example from './Example6'
+import Example from './example7/Example7'
 
 ReactDOM.render(<Example/>, document.getElementById('root'));
