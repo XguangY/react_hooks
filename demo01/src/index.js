95f58990115a9ba31f1334b19224a10ce5bba542
theone <xuguangyue@dtrobo.com>
2019-10-18 10:44:57 +0800

    自定义hooks

--- demo01/src/index.js
+++ demo01/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Example from './Example9'
+import Example from './Example10'
 
 ReactDOM.render(<Example/>, document.getElementById('root'));
